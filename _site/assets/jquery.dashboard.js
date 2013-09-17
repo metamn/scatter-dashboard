9089d9cd203c651c6fdc5dafd108650f9c31f58e
metamn <bartus.csongor@gmail.com>
2013-09-17 12:03:13 +0300

    Dashboard is now standalone

--- _site/assets/jquery.dashboard.js
+++ _site/assets/jquery.dashboard.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
   
+  // Navigate inside a section
+  $('section nav li').click(function() {
+    var index = $(this).index() + 2;
+    
+    var slide = $('article:nth-child(' + index + ')');
+    $("html, body").animate({ scrollTop: slide.offset().top }, "slow");
+    
+    $('section nav li').removeClass('active');
+    $(this).addClass('active');
+    
+    return false;
+  });
+  
+  
   // Hide / show dashboard
-  $('.dashboard #header #arrows span').click(function() {
-    $('.dashboard #header').toggleClass('hide', 700, "easeOutSine");
-    $('.dashboard main').toggleClass('expand', 700, "easeOutSine");
+  $('#header #arrows span').click(function() {
+    $(' #header').toggleClass('hide', 700, "easeOutSine");
+    $('main').toggleClass('expand', 700, "easeOutSine");
     
     $(this).removeClass('active');
     ($(this).index() == 0) ? $(this).next().addClass('active') : $(this).prev().addClass('active');
